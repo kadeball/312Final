5590186a17c5ddaf1942b0821ed85dc7e2f0e9a1
kadeball <kadeball@iu.edu>
2020-04-29 19:42:22 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Contact from "./components/Contact";
+import Flavors from "./components/Flavors";
+import homeImage1 from "./images/homeImage1.jpg";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    return (
+      <div className="App">
+          <Router>
+              <Navbar/>
+          <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route path='/Contact' component={Contact} exact/>
+            <Route path='/About' component={About} exact/>
+            <Route path='/Flavors' component={Flavors} exact/>
+          </Switch>
+        </Router>
+      </div>
   );
 }
 
